f8b9a140fee005e03d3fdc84fb01e35b87aeff6c
rharu <r.haruperi@hotmail.com>
2025-07-31 18:49:37 +0300

    fix : (strategy) Finalised swingline currency strength

--- app/strategy/backtrader.py
+++ app/strategy/backtrader.py
@@ -2,15 +2,20 @@ import pandas as pd
 import numpy as np
 from typing import Dict, Any
 from datetime import datetime, timezone, timedelta
+import sys
 from app.config.setup import *
 from app.backtesting import Backtest, Strategy
 from .base import BaseStrategy
 from .indicators import *
 import plotly.graph_objects as go
 from app.trading.risk_manager import *
-from .random import RandomStrategy
-from .naive_trend import NaiveTrendStrategy
+from .bench_random import RandomStrategy
+from .bench_hold_gold import HoldGoldStrategy
+from .bench_naive_trend import NaiveTrendStrategy
 from .harriet import HarrietStrategy
+from .trend_swingline_mtf import TrendSwinglineMTF
+from .trend_swingline_curr_strength import TrendSwinglineCurrStrength
+from app.util.helper import rearrange_trades_columns
 
 class BackTradeStrategy(Strategy):
     """
@@ -44,20 +49,19 @@ class BackTradeStrategy(Strategy):
         This method is called for each bar of the data.
         It checks the signal and places trades accordingly.
         """
-        # --- Risk Management ---
-        # def calculate_position_size(equity, risk_percent, price_distance):
-        #   risk_amount = equity * risk_percent / 100             
-        #   position_size = max(1000, (int(risk_amount / price_distance) // 1000) * 1000)
-        #   print(f"Position size: {position_size}, Equity: {equity}, Risk Amount: {risk_amount}, Price distance: {price_distance}")
-        #   return position_size
-        # Calculate position size
+
+        # --------------------------------------- Risk Management ---------------------------------------
+        # Filter to trade only during trading hours which is 09:00 to 18:00, else skip the trade
+        if self.data.index.hour[-1] < 9 or self.data.index.hour[-1] > 18:
+            return
+
         position_size = self.risk_manager.calculate_position_size(self.data.SL[-1], self.symbol_info)
-        # Convert position size to volume units
-        position_size_volume = position_size / self.point
+        position_size_volume = round(position_size / self.point)    # Convert position size to volume units
         sl = self.data.SL[-1] * self.point * 10
         tp = self.data.SL[-1] * 2 * self.point * 10
-      
 
+      
+        # --------------------------------------- Exit Logic (Optional) ---------------------------------------
         # Close any open position if the signal reverses
         # This part is optional but can be a valid approach
         # if self.position:
@@ -65,112 +69,224 @@ class BackTradeStrategy(Strategy):
         #         self.position.close()
         #     elif self.data.Signal[-1] == 1 and self.position.is_short:
         #         self.position.close()
-                
-        # --- Entry Logic ---
+
+
+
+        # --------------------------------------- Entry Logic ---------------------------------------
         # If there is no open position, check for a new signal
         if not self.position:
             # Buy signal
             if self.data.Signal[-1] == 1:
                 # Calculate SL and TP based on the current price
-                sl_price = self.data.Close[-1] - sl
-                tp_price = self.data.Close[-1] + tp
-                print(f"Buying at {self.data.Close[-1]} with SL: {sl_price} and TP: {tp_price}")
-                self.buy( sl=sl_price, tp=tp_price)
+                sl_price = round(self.data.Close[-1] - sl, self.symbol_info.digits)
+                tp_price = round(self.data.Close[-1] + tp, self.symbol_info.digits)
+                print(f"Buying at {self.data.Close[-1]} with SL: {sl_price} and TP: {tp_price} and size: {position_size_volume}")
+                self.buy(size=position_size_volume, sl=sl_price, tp=tp_price)
             
             # Sell signal
             elif self.data.Signal[-1] == -1:
                 # Calculate SL and TP based on the current price
-                sl_price = self.data.Close[-1] + sl
-                tp_price = self.data.Close[-1] - tp
-                print(f"Selling at {self.data.Close[-1]} with SL: {sl_price} and TP: {tp_price}")
-                self.sell( sl=sl_price, tp=tp_price)
+                sl_price = round(self.data.Close[-1] + sl, self.symbol_info.digits)
+                tp_price = round(self.data.Close[-1] - tp, self.symbol_info.digits)
+                print(f"Selling at {self.data.Close[-1]} with SL: {sl_price} and TP: {tp_price} and size: {position_size_volume}")
+                self.sell(size=position_size_volume, sl=sl_price, tp=tp_price)
 
 # --- Main Execution Block ---
 if __name__ == "__main__":
     
     # --- Get Data ---
+    # Initialize secondary MT5 client for index data (broker 3 - Purple Trading)
+    mt5_client_indices = MT5Client(config_path=DEFAULT_CONFIG_PATH, symbols=INDEX_SYMBOLS, broker=3)
+    
+    # Check secondary MT5 connection for indices
+    indices_account_info = mt5_client_indices.get_account_info()
+    if indices_account_info is None:
+        logger.error(f"Failed to connect to indices MT5 terminal. Error code: {mt5_client_indices.mt5.last_error()}")
+        index_dataframes = {}
+    else:
+        logger.info("Indices MT5 initialized successfully")
+        logger.info(f"Indices Login: {indices_account_info['login']} \tserver: {indices_account_info['server']}")
+
+        # Store index dataframes in a dictionary
+        index_dataframes = {}
+        for index in INDEX_SYMBOLS:
+            index_dataframes[index] = mt5_client_indices.fetch_data(index, "H1", start_pos=START_POS, end_pos=END_POS_HTF)
+            logger.info(f"Fetched {index} data: {index_dataframes[index].shape if index_dataframes[index] is not None else 'None'}")
+        
+    mt5_client_indices.shutdown()
+
+
     mt5_client = MT5Client(config_path=DEFAULT_CONFIG_PATH, symbols=FOREX_SYMBOLS, broker=BROKER)
-    symbol_info = mt5_client.get_symbol_info(TEST_SYMBOL)
+    for symbol in FOREX_SYMBOLS:
+        try:
+           
+            symbol_info = mt5_client.get_symbol_info(symbol)
+            
+            # Fetch data using keyword arguments
+            # data = mt5_client.fetch_data(symbol=TEST_SYMBOL, timeframe=DEFAULT_TIMEFRAME, start_date=START_DATE, end_date=END_DATE)
+            # data_htf = mt5_client.fetch_data(symbol=TEST_SYMBOL, timeframe=HIGHER_TIMEFRAME, start_date=START_DATE, end_date=END_DATE)
+            # data_core = mt5_client.fetch_data(symbol=TEST_SYMBOL, timeframe=CORE_TIMEFRAME, start_date=START_DATE_CORE, end_date=END_DATE)
 
-    data = mt5_client.fetch_data(TEST_SYMBOL, DEFAULT_TIMEFRAME, START_POS, END_POS)
-    data_htf = mt5_client.fetch_data(TEST_SYMBOL, HIGHER_TIMEFRAME, START_POS, END_POS_HTF)
-    data_core = mt5_client.fetch_data(TEST_SYMBOL, CORE_TIMEFRAME, START_POS, END_POS_D1)
-    
+            data = mt5_client.fetch_data(symbol=symbol, timeframe=DEFAULT_TIMEFRAME, start_pos=START_POS, end_pos=END_POS)
+            data_htf = mt5_client.fetch_data(symbol=symbol, timeframe=HIGHER_TIMEFRAME, start_pos=START_POS, end_pos=END_POS_HTF)
+            data_core = mt5_client.fetch_data(symbol=symbol, timeframe=CORE_TIMEFRAME, start_pos=START_POS, end_pos=END_POS_D1)
 
-    # -------------- Get Entry Signal for live trading ---dada
-    # random_strategy.data = data  # Set the data attribute
-    # entry_signal, entry_time = random_strategy.get_entry_signal(data.index[-1])
-
-    
-
-    # if entry_signal == 1:
-    #     buy_price = data.loc[entry_time]["Open"]
-    #     print(f"Buy at {buy_price}, entry time: {entry_time}")
-    # elif entry_signal == -1:
-    #     sell_price = data.loc[entry_time]["Open"]
-    #     print(f"Sell at {sell_price}, entry time: {entry_time}")
-    # else:
-    #     print("No signal")
-
-    # -----------------------------------------------------------
-    
+            if data is None or data_htf is None or data_core is None:
+                logger.error("Failed to fetch data for one or more timeframes.")
 
 
+        except Exception as e:
+            logger.error(f"Error fetching data: {e}")
 
-    # 2. Generate the trading signals
-    if data is not None and not data.empty:
-        strategy = 3
-        if strategy == 1:
-            random_strategy = RandomStrategy(mt5_client, parameters={})
-            data = random_strategy.get_features(data)
-            str_message, data = random_strategy.get_trade_parameters(data, data_core, 1, symbol_info)
-            print("SIGNAL DATA:")
-            print(data[data["Signal"] != 0])
-            print("TRADING PARAMETERS:")
-            print(str_message)
-        elif strategy == 2:
-            naive_trend_strategy = NaiveTrendStrategy(mt5_client, parameters={"fast_ema_period": 12, "slow_ema_period": 24, "bias_ema_period": 72})
-            data = naive_trend_strategy.get_features(data)
-            str_message, data = naive_trend_strategy.get_trade_parameters(data, data_core, 1, symbol_info)
-            print("SIGNAL DATA:")
-            print(data[data["Signal"] != 0])
-            print("TRADING PARAMETERS:")
-            print(str_message)
-        elif strategy == 3:
-            harriet_strategy = HarrietStrategy(mt5_client, parameters={"ht_min_dist": 5, "lt_min_dist": 2})
-            data = harriet_strategy.get_features(data, data_htf, ht_min_dist=5, lt_min_dist=2, symbol_info=symbol_info)
-            str_message, data = harriet_strategy.get_trade_parameters(data, data_core, 1, symbol_info)
-            print("SIGNAL DATA:")
-            print(data[data["Signal"] != 0])
-            print("TRADING PARAMETERS:")
-            print(str_message)
+
+     
+        
+
+        strategy = 5
+
+        # 2. Generate the trading signals
+        if data is not None and not data.empty and data_core is not None and not data_core.empty:
+            if strategy == 1:
+                random_strategy = RandomStrategy(mt5_client, parameters={"take_profit_pips": 80, "stop_loss_pips": 40})
+                data = random_strategy.get_features(data)
+                str_message, data = random_strategy.get_trade_parameters(data, data_core, 1, symbol_info)
+                if str_message is not None:
+                    print("SIGNAL DATA:")
+                    print(data[data["Signal"] != 0])
+                    print("TRADING PARAMETERS:")
+                    print(str_message)
+                else:
+                    logger.error("Error: Failed to get trade parameters for Random Strategy")
+
+            elif strategy == 2:
+                naive_trend_strategy = NaiveTrendStrategy(mt5_client, parameters={"fast_ema_period": 12, "slow_ema_period": 24, "bias_ema_period": 72, "take_profit_pips": 40, "stop_loss_pips": 20})
+                data = naive_trend_strategy.get_features(data)
+                str_message, data = naive_trend_strategy.get_trade_parameters(data, data_core, 1, symbol_info)
+                if str_message is not None:
+                    print("SIGNAL DATA:")
+                    print(data[data["Signal"] != 0])
+                    print("TRADING PARAMETERS:")
+                    print(str_message)
+                else:
+                    logger.error("Error: Failed to get trade parameters for Naive Trend Strategy")
+
+            elif strategy == 3:
+                harriet_strategy = HarrietStrategy(mt5_client, parameters={"ht_min_dist": 5, "lt_min_dist": 2})
+                data = harriet_strategy.get_features(data, data_htf, ht_min_dist=5, lt_min_dist=2, symbol_info=symbol_info)
+                str_message, data = harriet_strategy.get_trade_parameters(data, data_core, 1, symbol_info)
+                if str_message is not None:
+                    print("SIGNAL DATA:")
+                    print(data[data["Signal"] != 0])
+                    print("TRADING PARAMETERS:")
+                    print(str_message)
+                else:
+                    logger.error("Error: Failed to get trade parameters for Harriet Strategy")
+
+            elif strategy == 4:
+                trend_swingline_mtf_strategy = TrendSwinglineMTF(mt5_client, symbol_info, parameters={})
+                trigger_signal, data = trend_swingline_mtf_strategy.get_trigger_signal(data)
+
+                #if trigger_signal != 0:
+                if trigger_signal != 0:
+                    data = trend_swingline_mtf_strategy.get_features(data, data_htf)
+                    entry_signal, entry_time = trend_swingline_mtf_strategy.get_entry_signal(data)
+
+                    if entry_signal != 0:
+                        str_message, data = trend_swingline_mtf_strategy.get_trade_parameters(data, data_core, trigger_signal, symbol_info)
+                        if str_message is not None:
+                            print("SIGNAL DATA:")
+                            print(data)
+                            print("TRADING PARAMETERS:")
+                            print(str_message)
+                        else:
+                            logger.error("Error: Failed to get trade parameters for Trend Swingline MTF Strategy for symbol: {symbol}")
+                    else:
+                        logger.info(f"No entry signal found for Trend Swingline MTF Strategy for symbol: {symbol}")
+                else:
+                    logger.info(f"No trigger signal found for Trend Swingline MTF Strategy for symbol: {symbol}")
+
+            elif strategy == 5:
+                trend_swingline_curr_strength_strategy = TrendSwinglineCurrStrength(mt5_client, symbol_info, parameters={})
+                trigger_signal, data = trend_swingline_curr_strength_strategy.get_trigger_signal(data)
+
+                if trigger_signal != 0:
+                    # Get base and quote currency symbols
+                    base_currency_symbol = f"{symbol[:3]}X"
+                    quote_currency_symbol = f"{symbol[3:]}X"
+                    
+                    # Get the corresponding dataframes from the index_dataframes dictionary
+                    if base_currency_symbol in index_dataframes:
+                        df_base = index_dataframes[base_currency_symbol]
+                    else:
+                        logger.error(f"Base currency {base_currency_symbol} not found in index data")
+                        df_base = None
+                        
+                    if quote_currency_symbol in index_dataframes:
+                        df_quote = index_dataframes[quote_currency_symbol]
+                    else:
+                        logger.error(f"Quote currency {quote_currency_symbol} not found in index data")
+                        df_quote = None
+
+                    data = trend_swingline_curr_strength_strategy.get_features(data, df_base, df_quote)
+                    entry_signal, entry_time = trend_swingline_curr_strength_strategy.get_entry_signal(data)
+
+                    if entry_signal != 0:
+                        str_message, data = trend_swingline_curr_strength_strategy.get_trade_parameters(data, data_core, trigger_signal, symbol_info)
+                        if str_message is not None:
+                            print(f"SIGNAL DATA for {symbol}:")
+                            print(data)
+                            print("TRADING PARAMETERS:")
+                            print(str_message)
+                        else:
+                            logger.error("Error: Failed to get trade parameters for Trend Swingline Curr Strength Strategy for symbol: {symbol}")
+                    else:
+                        logger.info(f"No entry signal found for Trend Swingline Curr Strength Strategy for symbol: {symbol}")
+                else:
+                    logger.info(f"No trigger signal found for Trend Swingline Curr Strength Strategy for symbol: {symbol}")
 
         # 3. Run the backtest
-        print("Starting backtest...")
+        # print("Starting backtest...")
         
-        # Set class variables for the strategy
-        BackTradeStrategy.mt5_client = mt5_client
-        BackTradeStrategy.symbol_info = symbol_info
-        BackTradeStrategy.risk_manager = RiskManager(mt5_client)
+        # # Check if data is valid before running backtest
+        # if data is None or data.empty:
+        #     logger.error("Error: No valid data for backtesting. Please check your strategy parameters and data.")
+        #     sys.exit()
         
-        bt = Backtest(
-            data,
-            BackTradeStrategy,
-            cash=10000,
-            spread=SPREAD,
-            commission=.0002, # Example commission
-            margin=MARGIN,
-            exclusive_orders=True
-        )
+        # # Check if data has required columns for backtesting
+        # required_columns = ['Open', 'High', 'Low', 'Close', 'Volume', 'Signal']
+        # missing_columns = [col for col in required_columns if col not in data.columns]
+        # if missing_columns:
+        #     logger.error(f"Error: Missing required columns for backtesting: {missing_columns}")
+        #     sys.exit()
+        
+        # # Set class variables for the strategy
+        # BackTradeStrategy.mt5_client = mt5_client
+        # BackTradeStrategy.symbol_info = symbol_info
+        # BackTradeStrategy.risk_manager = RiskManager(mt5_client)
+        
+        # bt = Backtest(
+        #     data,
+        #     BackTradeStrategy,
+        #     cash=INITIAL_CAPITAL,
+        #     spread=SPREAD,
+        #     commission=.0002, # Example commission
+        #     margin=MARGIN,
+        #     exclusive_orders=True
+        # )
 
-        stats = bt.run()
-        print("\n--- Backtest Results ---")
-        print(stats)
-        print(stats._trades)
+        # stats = bt.run()
+        # print("\n--- Backtest Results ---")
+        # print(stats)
+        
+        # # Rearrange and print trades with desired column order
+        # rearranged_trades = rearrange_trades_columns(stats._trades)
+        # print("\n--- Trades (Rearranged Columns) ---")
+        # print(rearranged_trades)
 
-        # print("\nPlotting results...")
-        bt.plot(filename=os.path.join(BACKTESTS_DIR, "Backtrader"))
-        # bt.plot()
+
+        # # print("\nPlotting results and saving to CSV...")
+        # bt.plot(filename=os.path.join(BACKTESTS_DIR, "Backtrader"))
+        #data.to_csv(f"{DATA_DIR}/combined_signals.csv")
 
         # # Optimize the strategy with heatmap to see all results
         # print("Running optimization...")
---
+++ app/strategy/trend_swingline_curr_strength.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+from typing import Dict, Any, Optional
+from app.config.setup import *
+from .base import BaseStrategy
+
+
+logger = get_logger(__name__)
+
+class TrendSwinglineCurrStrength(BaseStrategy):
+    """
+    This class is the Trend Swingline Curr Strength.
+    """
+
+    def __init__(self, mt5_client: MT5Client, symbol_info: dict, parameters: Dict[str, Any]):
+        """
+        Initializes the strategy with the given parameters.
+
+        Args:
+            mt5_client (MT5Client): The MT5 client for data access.
+            symbol_info (dict): The symbol information.
+            parameters (Dict[str, Any]): A dictionary of parameters for the strategy.
+        """
+        super().__init__(mt5_client, parameters, symbol_info)
+
+    def get_features(self, data: pd.DataFrame, df_base: pd.DataFrame, df_quote: pd.DataFrame) -> pd.DataFrame:  
+        """
+        Generates Trend Swingline Curr Strength signals for the strategy.
+        Override this method to define strategy logic and signal generation.
+        Signals should be stored in data['Signal'] as +1 (buy), -1 (sell), 0 (hold).
+
+        Args:
+            data (pd.DataFrame): The historical price data. 
+            df_base (pd.DataFrame): The historical price data of the base currency. 
+            df_quote (pd.DataFrame): The historical price data of the quote currency. 
+        Returns:
+            pd.DataFrame: A DataFrame with Signal column.
+        """
+    
+        # First, calculate the swing values using SMC
+        df_base = self.smc.calculate_swingline(df_base)
+        df_quote = self.smc.calculate_swingline(df_quote)
+
+        # Keep only the following columns
+        df_base = df_base[["Close", "swingline", "swingvalue"]].copy()
+        df_quote = df_quote[["Close", "swingline", "swingvalue"]].copy()
+
+        # Shift by 1 row to avoid look ahead bias
+        # df_base["Close"] = df_base["Close"].shift(1)
+        # df_quote["Close"] = df_quote["Close"].shift(1)
+        # df_base["swingline"] = df_base["swingline"].shift(1)
+        # df_quote["swingline"] = df_quote["swingline"].shift(1)
+        # df_base["swingvalue"] = df_base["swingvalue"].shift(1)
+        # df_quote["swingvalue"] = df_quote["swingvalue"].shift(1)
+
+
+        df_base = df_base.dropna()
+        df_quote = df_quote.dropna()
+
+        df_base = df_base.rename(columns={'Close': 'base_Close','swingline': 'base_swingline', 'swingvalue': 'base_swingvalue'})
+        df_quote = df_quote.rename(columns={'Close': 'quote_Close','swingline': 'quote_swingline', 'swingvalue': 'quote_swingvalue'})
+
+        # Merge The df_base and the df_quote
+        curr_index_df =  df_base.merge(df_quote, left_index=True, right_index=True, how='left')
+
+        # Calculate HT signals on the unique HT data.
+        # Higher Low condition for Buy on HT
+        
+        # Currency signals
+        strong_buy = (curr_index_df['base_Close'] > curr_index_df['base_swingvalue']) & (curr_index_df['base_swingline'] == 1) & (curr_index_df['quote_Close'] < curr_index_df['quote_swingvalue']) & (curr_index_df['quote_swingline'] == -1)
+        mid_buy = (curr_index_df['base_Close'] > curr_index_df['base_swingvalue']) & (curr_index_df['base_swingline'] == 1) & (curr_index_df['quote_Close'] < curr_index_df['quote_swingvalue']) & (curr_index_df['quote_swingline'] == 1)
+        weak_buy = (curr_index_df['base_Close'] > curr_index_df['base_swingvalue']) & (curr_index_df['base_swingline'] == -1) & (curr_index_df['quote_Close'] < curr_index_df['quote_swingvalue']) & (curr_index_df['quote_swingline'] == 1)
+
+        strong_sell = (curr_index_df['base_Close'] < curr_index_df['base_swingvalue']) & (curr_index_df['base_swingline'] == -1) & (curr_index_df['quote_Close'] > curr_index_df['quote_swingvalue']) & (curr_index_df['quote_swingline'] == 1)
+        mid_sell = (curr_index_df['base_Close'] < curr_index_df['base_swingvalue']) & (curr_index_df['base_swingline'] == -1) & (curr_index_df['quote_Close'] > curr_index_df['quote_swingvalue']) & (curr_index_df['quote_swingline'] == -1)
+        weak_sell = (curr_index_df['base_Close'] < curr_index_df['base_swingvalue']) & (curr_index_df['base_swingline'] == 1) & (curr_index_df['quote_Close'] > curr_index_df['quote_swingvalue']) & (curr_index_df['quote_swingline'] == -1)
+
+        curr_index_df['Strength'] = 0
+        curr_index_df.loc[strong_buy, 'Strength'] = 3
+        curr_index_df.loc[mid_buy, 'Strength'] = 2
+        curr_index_df.loc[weak_buy, 'Strength'] = 1
+        curr_index_df.loc[strong_sell, 'Strength'] = -3
+        curr_index_df.loc[mid_sell, 'Strength'] = -2
+        curr_index_df.loc[weak_sell, 'Strength'] = -1
+        curr_index_df['Strength'] = curr_index_df['Strength'].shift(1)
+
+        curr_index_df = curr_index_df[["Strength"]]
+        data = data.merge(curr_index_df, left_index=True, right_index=True, how='left')
+        data["Strength"] = data["Strength"].ffill()
+
+        long_cond = (
+            (data["Trigger"] == 1) &
+            (data["Strength"] > 0)
+        )
+
+        short_cond = (
+            (data["Trigger"] == -1) &
+            (data["Strength"] < 0)
+        )
+
+        data["Signal"] = 0
+        data.loc[long_cond, "Signal"] = 1
+        data.loc[short_cond, "Signal"] = -1
+
+        # Drop these columns
+        data.drop(columns=["highest_low", "lowest_high"], inplace=True)
+        
+        return data
+
+    
